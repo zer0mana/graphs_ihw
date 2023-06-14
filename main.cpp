381bd593c4851c2d5212e14377804fa75d83290c
zer0mana <balabanovmm@gmail.com>
2023-06-14 20:26:53 +0300

    Добавил вывод в консоль

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ const int ITERATIONS = 1010;
 const int STEP = 50;
 const int ATTEMPTS = 5;
 std::ofstream out;
+std::ifstream in;
 
 void tester(std::function<pair<int, long long>(vector<vector<long>> graph, int n, int x, int y)> func, string func_name) {
     out << func_name << '\n';
@@ -58,15 +59,70 @@ void tester(std::function<pair<int, long long>(vector<vector<long>> graph, int n
     }
 }
 
+vector < string > strings;
+
+void custom_split(string str, char separator) {
+    int startIndex = 0, endIndex = 0;
+    for (int i = 0; i <= str.size(); i++) {
+
+        // If we reached the end of the word or the end of the input.
+        if (str[i] == separator || i == str.size()) {
+            endIndex = i;
+            string temp;
+            temp.append(str, startIndex, endIndex - startIndex);
+            strings.push_back(temp);
+            startIndex = endIndex + 1;
+        }
+    }
+}
+
 int main() {
     srand(time(NULL));
-    out.open(PATH);
+    // out.open(PATH);
 
-    tester(Dijkstra_algorithm, "Dijkstra_algorithm");
-    out << '\n';
-    tester(Floyd_Warshell_algorithm, "Floyd_Warshell_algorithm");
-    out << '\n';
-    tester(Ford_Bellman_function, "Ford_Bellman_function");
+    // tester(Dijkstra_algorithm, "Dijkstra_algorithm");
+    // out << '\n';
+    // tester(Floyd_Warshell_algorithm, "Floyd_Warshell_algorithm");
+    // out << '\n';
+    // tester(Ford_Bellman_function, "Ford_Bellman_function");
 
-    out.close();
+    // out.close();
+
+    in.open(PATH);
+
+    string algorithm;
+    string graph;
+    string edges;
+    string nodes;
+    string time;
+
+    string input;
+
+    for (int i = 0; i < 3; ++i) {
+        in >> algorithm;
+        cout << algorithm << '\n';
+
+        for (int j = 0; j < 3; ++j) {
+            in >> graph;
+            cout << graph << '\n';
+            cout << '\n';
+
+            for (int k = 10; k < ITERATIONS + 2 * STEP; k += STEP) {
+                in >> input;
+                custom_split(input, ';');
+                nodes += strings[0] + ',';
+                edges += strings[1] + ',';
+                time += strings[2] + ',';
+                strings.clear();
+            }
+
+            cout << nodes << '\n' << edges << '\n' << time << '\n';
+            nodes.clear();
+            edges.clear();
+            time.clear();
+            cout << '\n';
+        }
+    }
+
+    in.close();
 }
